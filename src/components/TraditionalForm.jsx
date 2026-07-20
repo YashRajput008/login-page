95270147aa5ac1898e510f0b8117028104735856
Yash Rajput <toyashrajput@gmail.com>
2026-07-20 16:53:02 -0400

    Added CSS and HTML elements

--- src/components/TraditionalForm.jsx
+++ src/components/TraditionalForm.jsx
@@ -1,11 +1,21 @@
-import './TraditionalForm.css'
+import "./TraditionalForm.css";
 
 const TraditionalForm = () => {
   return (
     <div>
-      <h2>TraditionalForm</h2>
+      <form className="form-container" action="">
+        <div className="form-item">
+          <label htmlFor="emailID">Email:</label>
+          <input id="emailID" name="emailID" type="text" />
+        </div>
+        <div className="form-item">
+          <label htmlFor="password">Password:</label>
+          <input id="password" name="password" type="text" />
+        </div>
+        <button className="form-btn">Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default TraditionalForm
+export default TraditionalForm;
