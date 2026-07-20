596937af7e5fcc07a8291cb938894ac532f8a71d
Yash Rajput <toyashrajput@gmail.com>
2026-07-20 16:18:07 -0400

    Created "TraditonalForm" component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import TraditionalForm from './TraditionalForm'
 
 function App() {
 
   return (
     <>
       <h1>Welcome to Login Page</h1>
+      <TraditionalForm/>
     </>
   )
 }
---
+++ src/components/TraditionalForm.jsx
@@ -0,0 +1,11 @@
+import './TraditionalForm.css'
+
+const TraditionalForm = () => {
+  return (
+    <div>
+      <h2>TraditionalForm</h2>
+    </div>
+  )
+}
+
+export default TraditionalForm
